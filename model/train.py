f94bd6f47fff31162248f0c4f72af20c3fe065bc
数据娃掘 <tifoit@126.com>
2024-10-18 05:40:29 +0800

    Update train.py

--- model/train.py
+++ model/train.py
@@ -1,1 +1,14 @@
+# train.py
 
+from model.gera_model import GeraModel, train_model
+from model.data_loader import load_data, preprocess_data
+
+def main():
+    data = load_data()
+    processed_data = preprocess_data(data)
+    model = GeraModel()
+    trained_model = train_model(model, processed_data)
+    print("模型训练完成。")
+
+if __name__ == "__main__":
+    main()
